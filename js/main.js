567242b9db9ead5c6c70ff3252cdfd1908f8d3d4
DimaG <funketrash@gmail.com>
2018-11-25 23:30:08 +0200

    add new page music

--- js/main.js
+++ js/main.js
@@ -46,6 +46,18 @@ $(document).ready(function(){
         nav :true,
         items:5
     });
+
+    $("ul.tabs-caption").on("click", "li:not(.active)", function() {
+        $(this)
+            .addClass("active")
+            .siblings()
+            .removeClass("active")
+            .closest("div.tabs")
+            .find("div.tabs-content")
+            .removeClass("active")
+            .eq($(this).index())
+            .addClass("active");
+    });
 });
 
 // window.onscroll = function() {scrollFunction()};
